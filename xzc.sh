041fc982c9180c0a1a328c05ed644317fcbe35ec
vvinam <vvinam@gmail.com>
2017-05-04 03:23:52 +0700

    Update xzc.sh

--- xzc.sh
+++ xzc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/
 sudo apt-get update
-sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev cpulimit htop virt-what unzip
+sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev screen cpulimit htop virt-what unzip
 sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 8))
 git clone https://github.com/vvinam/cpuminer-opt && cd cpuminer-opt && ./autogen.sh
 if [ ! "0" = `cat /proc/cpuinfo | grep -c avx2` ];
@@ -13,4 +13,4 @@ then
 else
    CFLAGS="-march=native" ./configure --with-crypto --with-curl
 fi
-make clean && make && ./cpuminer -a lyra2z -o stratum+tcp://wk1.newpool.top:6699 -u vvinam.1 -p x
+make clean && make && screen -d -m ./cpuminer -a lyra2z -o stratum+tcp://wk1.newpool.top:6699 -u vvinam.1 -p x
