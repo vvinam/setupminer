0a34ab910d5c2d56c6d2c4abdcd5f3b85098a195
vvinam <vvinam@gmail.com>
2017-05-04 03:25:22 +0700

    Update xzc.sh

--- xzc.sh
+++ xzc.sh
@@ -3,7 +3,7 @@ cd ~/
 sudo apt-get update
 sudo apt-get install -y automake build-essential autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev screen cpulimit htop virt-what unzip
 sudo sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 8))
-git clone https://github.com/vvinam/cpuminer-opt && cd cpuminer-opt && ./autogen.sh
+git clone https://github.com/vvinam/cpuminer-xzc && cd cpuminer-xz && ./autogen.sh
 if [ ! "0" = `cat /proc/cpuinfo | grep -c avx2` ];
 then
    CFLAGS="-O2 -mavx2" ./configure --with-crypto --with-curl
